d7225eaf4ba34c54af028bf1f7030d6692e096b1
Justin Ormont <spammyinboxhere+github@gmail.com>
2013-04-07 03:50:40 -0700

    Added lat/lng to flickr call

--- public/javascripts/flickr.js
+++ public/javascripts/flickr.js
@@ -4,21 +4,21 @@ var topPhotosIDs = [];
 function parsePhotoSearch(response, callback) {
 	"use strict";
 	
-	var responseMax = response.photos.photo.slice(0,maxCount);
-	
-	$.each(responseMax, function(i,v) {
-		topPhotosIDs.push(v.id);
-	});
-	
-	//console.log(topPhotosIDs);
+	if (response && response.photos && response.photos.photo) {
+		
+		var responseMax = response.photos.photo.slice(0,maxCount);
+		
+		$.each(responseMax, function(i,v) {
+			topPhotosIDs.push(v.id);
+		});
+	}
 	
 	callback(topPhotosIDs);
-	
 }
 
-function getBoundingBoxPhotos(callback,lat1,lng1,lat2,lng2) {
+function getBoundingBoxPhotos(callback, minimumLongitude, minimumLatitude, maximumLongitude, maximumLatitude) {
 	"use strict";
 	
-	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxCount +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=1.00140103605456%2C51.35108866688886%2C1.040175061070918%2C51.3681866301438&has_geo=1&sort=interestingness-desc&extras=geo&format=json&jsoncallback=?", function(response){parsePhotoSearch(response,callback);});
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxCount +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + minimumLongitude + "%2C" + minimumLatitude + "%2C" + maximumLongitude + "%2C" + maximumLatitude + "&has_geo=1&sort=interestingness-desc&extras=geo&format=json&jsoncallback=?", function(response){parsePhotoSearch(response,callback);});
 }
 
