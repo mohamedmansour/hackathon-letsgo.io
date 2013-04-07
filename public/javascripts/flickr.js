ca4c316c89678238cdbd17ed81bd0fb7d5b23df4
Justin Ormont <spammyinboxhere+github@gmail.com>
2013-04-07 03:32:30 -0700

    Added callback to getBoundingBoxPhotos(callback,lat1,lng1,lat2,lng2)

--- public/javascripts/flickr.js
+++ public/javascripts/flickr.js
@@ -1,7 +1,7 @@
 var maxCount=20;
 var topPhotosIDs = [];
 
-function parsePhotoSearch(response) {
+function parsePhotoSearch(response, callback) {
 	"use strict";
 	
 	var responseMax = response.photos.photo.slice(0,maxCount);
@@ -11,12 +11,14 @@ function parsePhotoSearch(response) {
 	});
 	
 	console.log(topPhotosIDs);
-
+	
+	callback(topPhotosIDs);
+	
 }
 
-function getBoundingBoxPhotos(lat1,lng1,lat2,lng2) {
+function getBoundingBoxPhotos(callback,lat1,lng1,lat2,lng2) {
 	"use strict";
 	
-	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxCount +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=1.00140103605456%2C51.35108866688886%2C1.040175061070918%2C51.3681866301438&has_geo=1&sort=interestingness-desc&extras=geo&format=json&jsoncallback=?", parsePhotoSearch);
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxCount +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=1.00140103605456%2C51.35108866688886%2C1.040175061070918%2C51.3681866301438&has_geo=1&sort=interestingness-desc&extras=geo&format=json&jsoncallback=?", function(response)){parsePhotoSearch, callback});
 }
 
