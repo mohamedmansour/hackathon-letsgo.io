2088048f67b812904e606281ef24d07dce0b8c9f
Justin Ormont <spammyinboxhere+github@gmail.com>
2013-04-07 10:07:33 -0700

    fixed the bad bbox

--- public/javascripts/flickr.js
+++ public/javascripts/flickr.js
@@ -21,4 +21,16 @@ function getBoundingBoxPhotos(minimumLongitude, minimumLatitude, maximumLongitud
 	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[1] + "%2C" + lats[0] + "%2C" + longs[0] + "%2C" + lats[1] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&jsoncallback=?", function(response){
 		parsePhotoSearch(response,callback);
 	});
+	
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[0] + "%2C" + lats[0] + "%2C" + longs[1] + "%2C" + lats[1] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&jsoncallback=?", function(response){
+		parsePhotoSearch(response,callback);
+	});
+	
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[1] + "%2C" + lats[1] + "%2C" + longs[0] + "%2C" + lats[0] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&jsoncallback=?", function(response){
+		parsePhotoSearch(response,callback);
+	});
+	
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[0] + "%2C" + lats[1] + "%2C" + longs[1] + "%2C" + lats[0] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&jsoncallback=?", function(response){
+		parsePhotoSearch(response,callback);
+	});
 }
