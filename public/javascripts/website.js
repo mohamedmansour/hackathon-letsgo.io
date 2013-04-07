6ac4c731db8e3bef23527dba5076827aa3844067
Joey Hiller <jhiller@JoeyMacBookPro.local>
2013-04-07 02:50:48 -0700

    Homepage commits

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -1,8 +1,11 @@
 function appActivate() {
     $("header").addClass("active");
+    $("#welcomeScreen").fadeOut(800);
+    $("#theApp").removeClass("obscured");
 }
 
 function fullPicture(image) {
     $("#bigPicture").addClass("active");
     $("#mapFrame").addClass("sidebar");
 }
+
