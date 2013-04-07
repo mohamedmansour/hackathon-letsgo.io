7b451c7d62174c893a134efaaa5d2073500550e3
Justin Ormont <spammyinboxhere+github@gmail.com>
2013-04-07 05:30:30 -0700

    Added function to rank photos

--- public/javascripts/flickr.js
+++ public/javascripts/flickr.js
@@ -1,28 +1,24 @@
-var maxCount=20;
-var topPhotos = [];
+
 
 function parsePhotoSearch(response, callback) {
 	"use strict";
 
 	if (response && response.photos && response.photos.photo) {
-
-		var responseMax = response.photos.photo.slice(0,maxCount);
-
-		$.each(responseMax, function(i,v) {
-			topPhotos.push(v);
-		});
+		getBestPlacesToVisit(response.photos.photo, callback);
 	}
-
-	callback(topPhotos);
+	else callback([]);
+	
 }
 
 function getBoundingBoxPhotos(minimumLongitude, minimumLatitude, maximumLongitude, maximumLatitude, callback) {
 	"use strict";
 
-	var longs = [minimumLongitude, maximumLongitude].sort(),
-		lats = [minimumLatitude, maximumLatitude].sort();
 
-	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxCount +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[0] + "%2C" + lats[0] + "%2C" + longs[1] + "%2C" + lats[1] + "&has_geo=1&sort=interestingness-desc&extras=geo&format=json&jsoncallback=?", function(response){
+	var longs = [minimumLongitude, maximumLongitude].sort(),
+		lats = [minimumLatitude, maximumLatitude].sort(),
+		maxPhotosToGetFromFlickr = 250;
+
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[0] + "%2C" + lats[0] + "%2C" + longs[1] + "%2C" + lats[1] + "&has_geo=1&sort=interestingness-desc&extras=geo&format=json&jsoncallback=?", function(response){
 		parsePhotoSearch(response,callback);
 	});
 }
---
+++ public/javascripts/getBestPlacesToVisit.js
@@ -0,0 +1,47 @@
+function getBestPlacesToVisit(topPhotos, callback) {
+	"use strict";
+	
+	var photoSortedByDistance;
+	
+	$.each(topPhotos, function(i,photo) {
+		photo.boringness = (i + 1)/topPhotos.length;
+		if (typeof photo.longitude !== "number" || typeof photo.latitude !== "number") { 
+			console.error("Photo does not have a latitude or longitiude."); 
+			return; 
+		}
+	});
+	
+	$.each(topPhotos, function(i,photo1) {
+		var totalDistanceFromOther = 0, distance;
+		$.each(topPhotos, function(j,photo2) {
+			if (photo1 !== photo2) {
+				distance = cityDistanceRealEarth({lat:photo1.latitude,lng:photo1.longitude},{lat:photo2.latitude,lng:photo2.longitude});
+				totalDistanceFromOther += distance;
+			}
+		});
+		
+		photo1.averageDistanceFromOtherPhotos = totalDistanceFromOther/(topPhotos.length-1);
+		photo1.weight = photo1.averageDistanceFromOtherPhotos * photo1.boringness * photo1.boringness;
+	});
+
+	photoSortedByDistance = topPhotos.slice(0)
+	photoSortedByDistance.sort(function(a,b) { return a.weight - b.weight; });
+	
+	callback(photoSortedByDistance);
+	
+}
+
+
+function cityDistanceRealEarth(location1,location2) {
+	var location1Lat = Math.PI * location1.lat/180;
+	var location1Lng = Math.PI * location1.lng/180;
+	var location2Lat = Math.PI * location2.lat/180;
+	var location2Lng = Math.PI * location2.lng/180;
+
+	var R = 3959; // mi
+	var d = Math.acos(Math.sin(location1Lat)*Math.sin(location2Lat) + 
+		Math.cos(location1Lat)*Math.cos(location2Lat) *
+		Math.cos(location2Lng-location1Lng)) * R;
+		
+	return d;
+}
