1b9c30206a1c6146f4341b7ff117395fb908b10d
Joey Hiller <jhiller@JoeyMacBookPro.local>
2013-04-07 05:05:53 -0700

    LOGO!

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -8,3 +8,16 @@ function fullPicture(image) {
     $("#bigPicture").addClass("active");
     $("#mapFrame").addClass("sidebar");
 }
+
+function logoSize() {
+    var offsert = $("#bigSearchBar").offset();
+    $("#introLogo").css({height: (offsert.top/3)*2, paddingTop: offsert.top/6});
+}
+
+$(document).ready(function() {
+    logoSize();
+})
+
+$(window).resize(function() {
+    logoSize();
+})
