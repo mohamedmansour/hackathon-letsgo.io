82b01f12c9b3346afe3d877dfed49dadb7bc66ef
Joey Hiller <jhiller@JoeyMacBookPro.local>
2013-04-07 09:20:37 -0700

    Background enhancements

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -32,3 +32,33 @@ $(document).on("click", ".mapImage", function() {
 	//var src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_l.jpg";
 	fullPicture(item.url_l);
 });
+
+
+
+//Check CSS3 Filters
+//From http://stackoverflow.com/questions/11047206/how-can-i-feature-detect-css-filters
+
+// function css3FilterFeatureDetect(enableWebkit) {
+//     if(enableWebkit === undefined) {
+//         enableWebkit = false;
+//     }
+//     el = document.createElement('div');
+//     el.style.cssText = (enableWebkit)?'-webkit-':'' + 'filter: blur(2px)';
+//     test1 = (el.style.length != 0);
+//     test2 = (
+//         document.documentMode === undefined
+//         || document.documentMode > 9
+//     );
+//     return test1 && test2;
+// }
+
+// alert(css3FilterFeatureDetect());
+
+if (document.body.style.filter !== undefined) {
+    console.log("YES");
+}
+else {
+    console.log("NO");
+}
+
+
