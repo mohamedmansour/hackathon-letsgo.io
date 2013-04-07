7f51cd803dc2eb3dc3f65fa02467096e53cda875
Justin Ormont <spammyinboxhere+github@gmail.com>
2013-04-07 11:43:14 -0700

    getty images

--- public/javascripts/flickr.js
+++ public/javascripts/flickr.js
@@ -19,19 +19,19 @@ function getBoundingBoxPhotos(minimumLongitude, minimumLatitude, maximumLongitud
 		lats = [minimumLatitude, maximumLatitude].sort(),
 		maxPhotosToGetFromFlickr = 100;
 
-	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[1] + "%2C" + lats[0] + "%2C" + longs[0] + "%2C" + lats[1] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&is_getty=" + getty + "&jsoncallback=?", function(response){
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[1] + "%2C" + lats[0] + "%2C" + longs[0] + "%2C" + lats[1] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&is_getty=" + (getty?'1':'0') + "&jsoncallback=?", function(response){
 		parsePhotoSearch(response,callback);
 	});
 	
-	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[0] + "%2C" + lats[0] + "%2C" + longs[1] + "%2C" + lats[1] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&is_getty=" + getty + "&jsoncallback=?", function(response){
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[0] + "%2C" + lats[0] + "%2C" + longs[1] + "%2C" + lats[1] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&is_getty=" + (getty?'1':'0') + "&jsoncallback=?", function(response){
 		parsePhotoSearch(response,callback);
 	});
 	
-	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[1] + "%2C" + lats[1] + "%2C" + longs[0] + "%2C" + lats[0] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&is_getty=" + getty + "&jsoncallback=?", function(response){
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[1] + "%2C" + lats[1] + "%2C" + longs[0] + "%2C" + lats[0] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&is_getty=" + (getty?'1':'0') + "&jsoncallback=?", function(response){
 		parsePhotoSearch(response,callback);
 	});
 	
-	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[0] + "%2C" + lats[1] + "%2C" + longs[1] + "%2C" + lats[0] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&is_getty=" + getty + "&jsoncallback=?", function(response){
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxPhotosToGetFromFlickr +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=" + longs[0] + "%2C" + lats[1] + "%2C" + longs[1] + "%2C" + lats[0] + "&has_geo=1&sort=interestingness-desc&extras=geo,url_l,url_s&format=json&is_getty=" + (getty?'1':'0') + "&jsoncallback=?", function(response){
 		parsePhotoSearch(response,callback);
 	});
 }
