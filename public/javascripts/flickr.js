197f689cb077c2dccaec1c1d592be1d21306af36
Justin Ormont <spammyinboxhere+github@gmail.com>
2013-04-07 03:33:36 -0700

    dsf

--- public/javascripts/flickr.js
+++ public/javascripts/flickr.js
@@ -19,6 +19,6 @@ function parsePhotoSearch(response, callback) {
 function getBoundingBoxPhotos(callback,lat1,lng1,lat2,lng2) {
 	"use strict";
 	
-	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxCount +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=1.00140103605456%2C51.35108866688886%2C1.040175061070918%2C51.3681866301438&has_geo=1&sort=interestingness-desc&extras=geo&format=json&jsoncallback=?", function(response)){parsePhotoSearch, callback});
+	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page="+ maxCount +"&api_key=6119f02e6572a0626d9f1df373ef2bb4&min_upload_date=1212710400&bbox=1.00140103605456%2C51.35108866688886%2C1.040175061070918%2C51.3681866301438&has_geo=1&sort=interestingness-desc&extras=geo&format=json&jsoncallback=?", function(response)){parsePhotoSearch(response,callback);});
 }
 
