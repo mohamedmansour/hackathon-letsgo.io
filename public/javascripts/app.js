718f9b29d24ac1988b0b3cef1ee38c87825847ea
Mohamed Mansour <hello@mohamedmansour.com>
2013-04-09 14:04:31 -0700

    Fix Key Enter events in IE
    It should be keypress not keyup

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -189,7 +189,7 @@ $('#lookup, #lookupHeader').click(function(e){
 });
 
 
-$('input').keyup(function(e){
+$('input').keypress(function(e){
 	if(e.keyCode === 13)
 		return fetchLocationAndLaunchQuery();
 });
