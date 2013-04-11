861f495cabbbf8538d326232c5b9aed8d5aab9dc
Joey Hiller <jthiller@gmail.com>
2013-04-10 20:46:19 -0700

    Restored SVG to the homescreen logo. Still need to recreate the header logo. Added Modernizr SVG detection to website.js.  Closes #20

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -1,17 +1,29 @@
 var isOpened = false, urlState = {q:null,from:null,wp:[],pids:[],dpid:null};
 
+/* Modernizr 2.6.2 (Custom Build) | MIT & BSD *
+ * Build: http://modernizr.com/download/#-svg */
+;window.Modernizr=function(a,b,c){function u(a){i.cssText=a}function v(a,b){return u(prefixes.join(a+";")+(b||""))}function w(a,b){return typeof a===b}function x(a,b){return!!~(""+a).indexOf(b)}function y(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:w(f,"function")?f.bind(d||b):f}return!1}var d="2.6.2",e={},f=b.documentElement,g="modernizr",h=b.createElement(g),i=h.style,j,k={}.toString,l={svg:"http://www.w3.org/2000/svg"},m={},n={},o={},p=[],q=p.slice,r,s={}.hasOwnProperty,t;!w(s,"undefined")&&!w(s.call,"undefined")?t=function(a,b){return s.call(a,b)}:t=function(a,b){return b in a&&w(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=q.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(q.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(q.call(arguments)))};return e}),m.svg=function(){return!!b.createElementNS&&!!b.createElementNS(l.svg,"svg").createSVGRect};for(var z in m)t(m,z)&&(r=z.toLowerCase(),e[r]=m[z](),p.push((e[r]?"":"no-")+r));return e.addTest=function(a,b){if(typeof a=="object")for(var d in a)t(a,d)&&e.addTest(d,a[d]);else{a=a.toLowerCase();if(e[a]!==c)return e;b=typeof b=="function"?b():b,typeof enableClasses!="undefined"&&enableClasses&&(f.className+=" "+(b?"":"no-")+a),e[a]=b}return e},u(""),h=j=null,e._version=d,e}(this,this.document);
+
+if(!Modernizr.svg) {
+	$('img[src*="svg"]').attr('src', function() {
+		return $(this).attr('src').replace('.svg', '.png');
+	});
+}
+
+
+
 function fullPicture(image) {
-    $("#bigPicture").css({'background-image':'url('+image+')'}).addClass("active");
-    $("#mapFrame").addClass("sidebar");
-    
+	$("#bigPicture").css({'background-image':'url('+image+')'}).addClass("active");
+	$("#mapFrame").addClass("sidebar");
+
 	if (!isOpened) _gaq.push(['_trackEvent', 'Photo', 'OpenedFullPictureViewer', image]);
 	isOpened = true;
 }
 
 function smallPicture() {
-    $("#bigPicture").removeClass("active");
-    $("#mapFrame").removeClass("sidebar");
-    if (isOpened) _gaq.push(['_trackEvent', 'Photo', 'ClosedFullPictureViewer']);
+	$("#bigPicture").removeClass("active");
+	$("#mapFrame").removeClass("sidebar");
+	if (isOpened) _gaq.push(['_trackEvent', 'Photo', 'ClosedFullPictureViewer']);
 	isOpened = false;
 }
 
@@ -49,6 +61,7 @@ function getLink() {
 	return link;
 }
 
+// Responsive Logo for the home screen splash.
 function logoSize() {
     var offsert = $("#bigSearchBar").offset();
     $("#introLogo").css({height: (offsert.top/3)*2, paddingTop: offsert.top/6});
@@ -91,32 +104,6 @@ $(document).on("keyup", function(e) {
 	if (e.keyCode === 27) smallPicture();
 });
 
-
-//Check CSS3 Filters
-//From http://stackoverflow.com/questions/11047206/how-can-i-feature-detect-css-filters
-
-// function css3FilterFeatureDetect(enableWebkit) {
-//     if(enableWebkit === undefined) {
-//         enableWebkit = false;
-//     }
-//     el = document.createElement('div');
-//     el.style.cssText = (enableWebkit)?'-webkit-':'' + 'filter: blur(2px)';
-//     test1 = (el.style.length != 0);
-//     test2 = (
-//         document.documentMode === undefined
-//         || document.documentMode > 9
-//     );
-//     return test1 && test2;
-// }
-
-// alert(css3FilterFeatureDetect());
-
-// if (document.body.style.filter !== undefined) {
-//     console.log("YES");
-// }
-// else {
-//     console.log("NO");
-// }
 
 
 
